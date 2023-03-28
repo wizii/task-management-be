eb0a8277af254d74ba120fc81a5d7ebd9499408c
wizii <wizim393@gmail.com>
2023-03-28 19:37:46 +0400

    generate spec + update readme

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
+  const config = new DocumentBuilder()
+    .setTitle('Task Management API')
+    .setDescription('Task Management API with CRUD functionality')
+    .setVersion('1.0')
+    .build();
+
   const app = await NestFactory.create(AppModule);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   app.enableCors();
   await app.listen(8000);
 }
